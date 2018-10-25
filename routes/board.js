c5a875206a27323be63a95ffa73f162c06995d62
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-10-25 21:19:22 +0900

    2018.10.25일 작업분 저장
    1) 임시 DB 대체용 json 생성
    2) 나머지 페이지 만듬

--- routes/board.js
+++ routes/board.js
@@ -1,31 +1,69 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
-var postTitle = "a";
-var postContent = "a";
-var postTime = "a";
-var postHits = 10;
-var postWriter = "a";
-var visible = 1;
+
+// DB 대체
+var dbData = require('../public/test/db.json');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/board/view");
+  res.redirect("/board/lists");
 });
 
 router.get('/view', function(req, res, next) {
-  if (visible == 0) {
+  res.redirect("/board/lists");
+});
+router.get('/view/:id', function(req, res, next) {
+  // DB 불러오기
+  console.log(req.params.id);
+  if (typeof req.params.id === "undefined") {
+    res.redirect("/board/lists");
+  }
+  postWriter = dbData['writer']
+  if (dbData['visible'] == 0) {
     postWriter = "익명";
   }
-  console.log(postWriter);
-  res.render('board/view', { title: postTitle, content: postContent, time: postTime, hits: postHits, writer: postWriter });
+  console.log("Board System Working");
+  res.render('board/view', { title: dbData['title'], content: dbData['content'], time: dbData['time'], hits: dbData['hits'], writer: postWriter, boardNum: req.params.id });
 });
 router.get('/lists', function(req, res, next) {
-  if (visible == 0) {
-    postWriter = "익명";
-  }
-  console.log(postWriter);
-  res.render('board/lists', { title: postTitle, content: postContent, time: postTime, hits: postHits, writer: postWriter });
+  var board = req.query.flag;
+  console.log("Board System Working");
+  res.render('board/lists', { title: dbData['title'], content: dbData['content'], time: dbData['time'], hits: dbData['hits'], writer: dbData['writer'] });
 });
+router.get('/write', function(req, res, next) {
+  console.log("Board System Working");
+  res.render('board/write', {});
+});
+router.get('/modify', function(req, res, next) {
+  console.log("Board System Working");
+  res.render('board/modify', { title: dbData['title'], content: dbData['content'] });
+});
+router.get('/delete', function(req, res, next) {
+  res.redirect("/board/lists");
+});
+router.get('/delete/:id', function(req, res, next) {
+  if (typeof req.params.id === "undefined") {
+    res.redirect("/board/lists");
+  }
+  res.render('board/delete', { title: dbData['title'], nid: req.params.id });
+});
+
+router.post('/deleteDo', function(req, res, next) {
+  if (typeof req.body.id === "undefined") {
+    res.send("<script type='text/javascript'>window.alert('본문이 입력되지 않았습니다.');window.location=('/board/lists');</script>");
+  }
+
+  // DB 처리
+  res.redirect("/board/lists");
+})
+
+router.post('/writeDo', function(req, res, next) {
+  if (typeof req.body.id === "undefined") {
+    res.send("<script type='text/javascript'>window.alert('본문이 입력되지 않았습니다.');window.location=('/board/lists');</script>");
+  }
+  res.redirect("/board/lists");
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { target1: '/board/view', target2: '/board/write', target3: '/board/modify', target4: '/board/lists' });
+  res.render('index', { target1: '/board/view', target2: '/board/write', target3: '/board/modify', target4: '/board/lists', target5: '/board/delete' });
 });
 
 module.exports = router;
