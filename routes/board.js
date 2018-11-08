434b4bf7a45c9387fb16dfd73635f0fc4de308fa
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-11-08 21:01:11 +0900

    디버깅 안한 DB 연결
    저거 오류뜨면 분명히 JSON 처리 문제일거다

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 var express = require('express');
 var request = require('request');
+var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
-
 // DB 대체
-var dbData = require('../public/test/db.json');
+//var dbData = require('../public/test/db.json');
 
+(async function() {
+  // Connection URL
+  const url = 'mongodb://localhost:27017/board';
+  // Database Name
+  const dbName = 'board';
+  const client = new MongoClient(url);
+
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+
+    const db = client.db(dbName);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  //client.close();
+})();
 
 /* GET home page. */
 router.get('/:board', function(req, res, next) {
@@ -13,7 +33,16 @@ router.get('/:board', function(req, res, next) {
   if (typeof req.params.board === "undefined" || (board !== 'freeboard' && board !== 'notice' && board !== 'storage')) {
     board = "freeboard";
   }
-  // DB 내용 가져오기
+  if (board === "error") {
+    res.render('board/error');
+    return;
+  }
+  var dbData = db.board.find({board: board});
+  for (var i = 0; i < dbdata.length; i++) {
+    if (dbdata[i]['visible'] === 0) {
+      dbdata[i]['writer_id'] = "익명";
+    }
+  }
   res.render('board/lists', { board: board, list: dbData });
 });
 
@@ -22,14 +51,25 @@ router.get('/:board/:id', function(req, res, next) {
   var id = req.params.id;
   if (typeof req.params.board === "undefined" || (board !== 'freeboard' && board !== 'notice' && board !== 'storage')) {
     res.redirect("/board/freeboard");
+    return;
   }
   if (typeof req.params.id === "undefined") {
     res.redirect("/board/" + req.params.board);
+    return;
   }
   if (id === "write") {
     res.render('board/write', { board: board });
   } else {
-    postWriter = dbData['writer']
+    var dbData;
+    try {
+      dbData = db.board.findOne({visible: 1, board: board, board_num: id});
+    } catch (e) {
+      res.redirect('/board/error');
+      return;
+    }
+
+
+    var postWriter = dbData['writer']
     if (dbData['visible'] == 0) {
       postWriter = "익명";
     }
@@ -42,12 +82,22 @@ router.get('/:board/:id/:mode', function(req,res,next) {
   var board = req.params.board;
   if (typeof req.params.board === "undefined" || (board !== 'freeboard' && board !== 'notice' && board !== 'storage')) {
     res.redirect("/board/freeboard");
+    return;
   }
   if (typeof req.params.id === "undefined") {
     res.redirect("/board/freeboard");
+    return;
   }
   if (typeof req.params.board === "undefined" || (board !== 'freeboard' && board !== 'notice' && board !== 'storage')) {
     res.redirect("/board/freeboard");
+    return;
+  }
+  var dbData;
+  try {
+    dbData = db.board.findOne({visible: 1, board: board, board_num: id});
+  } catch (e) {
+    res.redirect('/board/error');
+    return;
   }
   switch (req.params.mode) {
     case "edit":
@@ -60,24 +110,28 @@ router.get('/:board/:id/:mode', function(req,res,next) {
   }
 });
 
-
-router.get('/write', function(req, res, next) {
-  console.log("Board System Working");
-
-});
-
-router.post('/deleteDo', function(req, res, next) {
-  if (typeof req.body.id === "undefined") {
-    res.send("<script type='text/javascript'>window.alert('본문이 입력되지 않았습니다.');window.location=('/board/lists');</script>");
+router.get('/deleteDo/:id', function(req, res, next) {
+  if (typeof req.params.id === "undefined") {
+    res.send("<script type='text/javascript'>window.alert('ERROR.');window.location=('/board/lists');</script>");
+    return;
   }
-
+  db.board.remove({"board_num": req.params.id})
   // DB 처리
   res.redirect("/board/lists");
 })
 
-router.post('/writeDo', function(req, res, next) {
-  if (typeof req.body.id === "undefined") {
-    res.send("<script type='text/javascript'>window.alert('본문이 입력되지 않았습니다.');window.location=('/board/lists');</script>");
+router.post('/writeDo/:id', function(req, res, next) {
+  if (typeof req.params.id === "undefined") {
+    res.send("<script type='text/javascript'>window.alert('ERROR.');window.location=('/board/lists');</script>");
+  }
+  var dbData;
+  try {
+    dbData = db.meta.findOne({info: 'board'});
+    db.meta.update({info:'board'}, {$set: { dbData['count'] + 1 }});
+    db.board.insertOne({board: req.body.board, board_num: dbData['count'] + 1});
+  } catch (e) {
+    res.send("<script type='text/javascript'>window.alert('ERROR.');window.location=('/board/lists');</script>");
+    return;
   }
   res.redirect("/board/lists");
 })
